28fef44eebb96909ffae56ae40643570344d538f
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-07-21 13:05:22 +0800

    wip: withdrawal script

---
+++ scripts/test/1_deposit.ts
@@ -0,0 +1,51 @@
+import { ethers, network } from 'hardhat'
+import { readDeployedContracts } from '../utils/io'
+import { getRandomPubkey, getRandomSalt } from '../utils/rand'
+import { getPubkeySaltHash } from '../utils/hash'
+import { sleep } from '../utils/sleep'
+
+if (network.name !== 'sepolia') {
+	throw new Error('This script should be run on sepolia network')
+}
+
+async function main() {
+	const deployedContracts = await readDeployedContracts()
+	if (!deployedContracts.liquidity) {
+		throw new Error('No liquidity contract found')
+	}
+	const liquidity = await ethers.getContractAt(
+		'Liquidity',
+		deployedContracts.liquidity,
+	)
+
+	// deposit
+	const pubkey = getRandomPubkey() // intmax address of user
+	const salt = getRandomSalt() // random salt
+	const pubkeySaltHash = getPubkeySaltHash(pubkey, salt)
+	const tx = await liquidity.depositETH(pubkeySaltHash, {
+		value: ethers.parseEther('0.0001'),
+	})
+	console.log('deposit tx hash:', tx.hash)
+	await tx.wait()
+
+	await sleep(60000)
+
+	// analyze
+	const tx1 = await liquidity.analyzeDeposits(1, [])
+	console.log('analyze tx hash:', tx1.hash)
+	await tx1.wait()
+
+	await sleep(60000)
+
+	// relay
+	const tx2 = await liquidity.relayDeposits(1, 400_000, {
+		value: ethers.parseEther('0.1'), // will be refunded automatically
+	})
+	console.log('relay tx hash:', tx2.hash)
+	await tx2.wait()
+}
+
+main().catch((error) => {
+	console.error(error)
+	process.exitCode = 1
+})
---
+++ scripts/test/2_withdrawal.ts
@@ -0,0 +1,22 @@
+import { ethers, network } from 'hardhat'
+import { readDeployedContracts } from '../utils/io'
+
+if (network.name !== 'sepoliascroll') {
+	throw new Error('This script should be run on sepoliascroll network')
+}
+
+async function main() {
+	const deployedContracts = await readDeployedContracts()
+	if (!deployedContracts.withdrawal) {
+		throw new Error('No withdrawal contract found')
+	}
+	const withdrawal = await ethers.getContractAt(
+		'Withdrawal',
+		deployedContracts.withdrawal,
+	)
+}
+
+main().catch((error) => {
+	console.error(error)
+	process.exitCode = 1
+})
