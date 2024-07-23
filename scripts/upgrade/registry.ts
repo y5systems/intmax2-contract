6801886af382e3715ec1d1ee0ce984a346fea6c2
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-07-23 15:23:43 +0800

    add registry upgrade

---
+++ scripts/upgrade/registry.ts
@@ -0,0 +1,25 @@
+import { ethers, upgrades, network } from 'hardhat'
+import { readDeployedContracts } from '../utils/io'
+
+if (network.name !== 'scrollSepolia') {
+	throw new Error('This script should be run on scrollSepolia network')
+}
+
+async function main() {
+	const deployedContracts = await readDeployedContracts()
+	if (!deployedContracts.blockBuilderRegistry) {
+		throw new Error('blockBuilderRegistry contract should be deployed')
+	}
+	const newImplementationFactory = await ethers.getContractFactory(
+		'BlockBuilderRegistry',
+	)
+	await upgrades.upgradeProxy(
+		deployedContracts.blockBuilderRegistry,
+		newImplementationFactory,
+	)
+}
+
+main().catch((error) => {
+	console.error(error)
+	process.exitCode = 1
+})
