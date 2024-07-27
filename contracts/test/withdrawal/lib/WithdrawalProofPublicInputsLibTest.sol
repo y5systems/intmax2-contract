6bcfdc07514c598fce12a8571c00ba4aa583e003
“speed-daemon” <spead.deamon.20000101@gmail.com>
2024-07-27 22:49:43 +0900

    add test and version up library

---
+++ contracts/test/withdrawal/lib/WithdrawalProofPublicInputsLibTest.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {WithdrawalProofPublicInputsLib} from "../../../withdrawal/lib/WithdrawalProofPublicInputsLib.sol";
+
+contract WithdrawalProofPublicInputsLibTest {
+	function getHash(
+		bytes32 lastWithdrawalHash,
+		address withdrawalAggregator
+	) external pure returns (bytes32) {
+		WithdrawalProofPublicInputsLib.WithdrawalProofPublicInputs
+			memory inputs = WithdrawalProofPublicInputsLib
+				.WithdrawalProofPublicInputs({
+					lastWithdrawalHash: lastWithdrawalHash,
+					withdrawalAggregator: withdrawalAggregator
+				});
+
+		return WithdrawalProofPublicInputsLib.getHash(inputs);
+	}
+
+	function createInputs(
+		bytes32 lastWithdrawalHash,
+		address withdrawalAggregator
+	)
+		external
+		pure
+		returns (
+			WithdrawalProofPublicInputsLib.WithdrawalProofPublicInputs memory
+		)
+	{
+		return
+			WithdrawalProofPublicInputsLib.WithdrawalProofPublicInputs({
+				lastWithdrawalHash: lastWithdrawalHash,
+				withdrawalAggregator: withdrawalAggregator
+			});
+	}
+}
