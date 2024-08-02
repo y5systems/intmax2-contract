e822d656c8d18239c69dcc81ee8030ffb2111ff0
Baby Bear <132609968+smallbabybear@users.noreply.github.com>
2024-08-02 08:36:53 +0200

    Add deploy script

---
+++ contracts/test/TestNFT.sol
@@ -0,0 +1,12 @@
+/// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TestNFT is ERC721 {
+    constructor() ERC721("TestNFT", "TST") {
+        for (uint256 i = 0; i < 10; i++) {
+            _mint(msg.sender, i);
+        }
+    }
+}
