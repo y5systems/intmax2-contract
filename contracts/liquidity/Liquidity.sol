1c657e58ef581a4bd2c0780f7e40971880367d54
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-08-26 12:53:12 +0900

    move contribution to `depositNativeToken`

--- contracts/liquidity/Liquidity.sol
+++ contracts/liquidity/Liquidity.sol
@@ -77,6 +77,15 @@ contract Liquidity is
 		}
 		uint32 tokenIndex = _getNativeTokenIndex();
 		_deposit(_msgSender(), recipientSaltHash, tokenIndex, msg.value);
+
+		if (msg.value == 1 ether) {
+			// Record contribution only 1 eth.
+			contribution.recordContribution(
+				keccak256("DEPOSIT_1ETH"),
+				_msgSender(),
+				1
+			);
+		}
 	}
 
 	function depositERC20(
@@ -232,14 +241,6 @@ contract Liquidity is
 			.Deposit(recipientSaltHash, tokenIndex, amount)
 			.getHash();
 		uint256 depositId = depositQueue.enqueue(depositHash, sender);
-		if (tokenIndex == 0 && amount == 1 ether) {
-			// Record contribution only 1 eth.
-			contribution.recordContribution(
-				keccak256("DEPOSIT_1ETH"),
-				_msgSender(),
-				1
-			);
-		}
 		emit Deposited(
 			depositId,
 			sender,
