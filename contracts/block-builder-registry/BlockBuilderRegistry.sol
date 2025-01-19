f5c4744b6a45d0f130915cd08ecf65c5e3f65037
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-01-19 10:48:52 +0700

    update block builder registry

--- contracts/block-builder-registry/BlockBuilderRegistry.sol
+++ contracts/block-builder-registry/BlockBuilderRegistry.sol
@@ -10,9 +10,6 @@ contract BlockBuilderRegistry is
 	UUPSUpgradeable,
 	IBlockBuilderRegistry
 {
-	mapping(address => BlockBuilderInfo) public blockBuilders;
-	address[] private blockBuilderAddresses;
-
 	/// @custom:oz-upgrades-unsafe-allow constructor
 	constructor() {
 		_disableInitializers();
@@ -23,67 +20,8 @@ contract BlockBuilderRegistry is
 		__UUPSUpgradeable_init();
 	}
 
-	function updateBlockBuilder(string calldata url) external {
-		if (bytes(url).length == 0) {
-			revert URLIsEmpty();
-		}
-		BlockBuilderInfo memory info = blockBuilders[_msgSender()];
-		if (bytes(info.blockBuilderUrl).length == 0) {
-			blockBuilderAddresses.push(_msgSender());
-		}
-		info.blockBuilderUrl = url;
-		info.isActive = true;
-		blockBuilders[_msgSender()] = info;
-
-		emit BlockBuilderUpdated(_msgSender(), url);
-	}
-
-	function stopBlockBuilder() external {
-		// Remove the block builder information.
-		BlockBuilderInfo storage info = blockBuilders[_msgSender()];
-		if (bytes(info.blockBuilderUrl).length == 0) {
-			revert BlockBuilderNotFound();
-		}
-		info.isActive = false;
-
-		emit BlockBuilderStopped(_msgSender());
-	}
-
-	function isActiveBlockBuilder(
-		address blockBuilder
-	) external view returns (bool) {
-		return blockBuilders[blockBuilder].isActive;
-	}
-
-	function getActiveBlockBuilders()
-		external
-		view
-		returns (BlockBuilderInfoWithAddress[] memory)
-	{
-		uint256 blockBuilderLength = blockBuilderAddresses.length;
-		uint256 counter = 0;
-		for (uint256 i = 0; i < blockBuilderLength; i++) {
-			if (blockBuilders[blockBuilderAddresses[i]].isActive) {
-				counter++;
-			}
-		}
-		BlockBuilderInfoWithAddress[]
-			memory activeBlockBuilders = new BlockBuilderInfoWithAddress[](
-				counter
-			);
-		uint256 index = 0;
-		for (uint256 i = 0; i < blockBuilderLength; i++) {
-			address blockBuilderAddress = blockBuilderAddresses[i];
-			BlockBuilderInfo memory info = blockBuilders[blockBuilderAddress];
-			if (info.isActive) {
-				activeBlockBuilders[index] = BlockBuilderInfoWithAddress({
-					blockBuilderAddress: blockBuilderAddress,
-					info: info
-				});
-				index++;
-			}
-		}
-		return activeBlockBuilders;
+	function emitHeartbeat(string calldata url) external {
+		emit BlockBuilderHeartbeat(_msgSender(), url);
 	}
 
 	function _authorizeUpgrade(address) internal override onlyOwner {}
--- contracts/block-builder-registry/IBlockBuilderRegistry.sol
+++ contracts/block-builder-registry/IBlockBuilderRegistry.sol
@@ -5,65 +5,8 @@ interface IBlockBuilderRegistry {
 	/// @notice Error thrown when trying to register a block builder with an empty URL
 	error URLIsEmpty();
 
-	/// @notice Error thrown when trying to stop a block builder that is not adding
-	error BlockBuilderNotFound();
-
-	/// @notice Event emitted when a block builder is updated
+	/// @notice Event emitted when a block builder is online
 	/// @param blockBuilder The address of the updated block builder
-	/// @param url The new URL of the block builder
-	event BlockBuilderUpdated(address indexed blockBuilder, string url);
-
-	/// @notice Event emitted when a block builder stops operations
-	/// @param blockBuilder The address of the block builder that stopped
-	event BlockBuilderStopped(address indexed blockBuilder);
-
-	/**
-	 * @notice Block builder information.
-	 * @param blockBuilderUrl The URL or IP address of Block builder.
-	 * @param isActive active flag.
-	 */
-	struct BlockBuilderInfo {
-		string blockBuilderUrl;
-		bool isActive;
-	}
-
-	/**
-	 * @notice Block builder info with address.
-	 * @param blockBuilderAddress The address of the block builder.
-	 * @param info The block builder information.
-	 */
-	struct BlockBuilderInfoWithAddress {
-		address blockBuilderAddress;
-		BlockBuilderInfo info;
-	}
-
-	/**
-	 * @notice Update block builder.
-	 * @dev This method is used to register or update the URL or IP address of the block builder.
-	 * @param url The URL or IP address of Block builder.
-	 */
-	function updateBlockBuilder(string memory url) external;
-
-	/**
-	 * @notice Declare that the block builder has stopped.
-	 */
-	function stopBlockBuilder() external;
-
-	/**
-	 * @notice Check if the block builder is active.
-	 * @param blockBuilder The address of the block builder.
-	 * @return True if the block builder is active.
-	 */
-	function isActiveBlockBuilder(
-		address blockBuilder
-	) external view returns (bool);
-
-	/**
-	 * @notice Get the block builder information of active block builders.
-	 * @return The block builder information.
-	 */
-	function getActiveBlockBuilders()
-		external
-		view
-		returns (BlockBuilderInfoWithAddress[] memory);
+	/// @param url The URL of the block builder
+	event BlockBuilderHeartbeat(address indexed blockBuilder, string url);
 }
