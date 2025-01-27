f313df5af486c03bd0d842eb974c118607cf46ac
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-01-27 15:13:38 +0700

    update contract

--- contracts/block-builder-registry/IBlockBuilderRegistry.sol
+++ contracts/block-builder-registry/IBlockBuilderRegistry.sol
@@ -2,11 +2,10 @@
 pragma solidity 0.8.27;
 
 interface IBlockBuilderRegistry {
-	/// @notice Error thrown when trying to register a block builder with an empty URL
-	error URLIsEmpty();
-
 	/// @notice Event emitted when a block builder is online
 	/// @param blockBuilder The address of the updated block builder
 	/// @param url The URL of the block builder
 	event BlockBuilderHeartbeat(address indexed blockBuilder, string url);
+
+	function emitHeartbeat(string calldata url) external;
 }
