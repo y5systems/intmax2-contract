5d5eed42540a2663536f3d7c8accea00daa32f84
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-02-10 19:50:56 +0700

    upgrade claim

---
+++ scripts/upgrade/claim.ts
@@ -0,0 +1,27 @@
+import { ethers, upgrades, network } from 'hardhat'
+import { readDeployedContracts } from '../utils/io'
+
+if (network.name !== 'scrollSepolia') {
+	throw new Error('This script should be run on scrollSepolia network')
+}
+
+async function main() {
+	const deployedContracts = await readDeployedContracts()
+	if (!deployedContracts.claim) {
+		throw new Error('claim contract should be deployed')
+	}
+
+	const newImplementationFactory = await ethers.getContractFactory('Claim')
+
+	await upgrades.forceImport(deployedContracts.claim, newImplementationFactory)
+
+	await upgrades.upgradeProxy(
+		deployedContracts.claim,
+		newImplementationFactory,
+	)
+}
+
+main().catch((error) => {
+	console.error(error)
+	process.exitCode = 1
+})
