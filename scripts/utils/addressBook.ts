051e2f7fc40b4828d028ef9617da1f55e5e057f2
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-03-25 16:40:21 +0700

    add contract address to addressBook

--- scripts/utils/addressBook.ts
+++ scripts/utils/addressBook.ts
@@ -1,22 +1,31 @@
-import { ethers, network } from 'hardhat'
+import { network } from 'hardhat'
 import { readDeployedContracts } from './io'
 
 export const getL1MessengerAddress = async () => {
 	if (network.name === 'sepolia') {
-		return '0x50c7d3e7f7c656493D1D76aaa1a836CedfCBB16A' // real address
+		return '0x50c7d3e7f7c656493D1D76aaa1a836CedfCBB16A'
 	}
+	if (network.name === 'mainnet') {
+		return '0x6774Bcbd5ceCeF1336b5300fb5186a12DDD8b367'
+	}
+
+	// Get mock l1 messenger address from deployed contracts
 	const deployedContracts = await readDeployedContracts()
 	if (deployedContracts.mockL1ScrollMessenger) {
 		return deployedContracts.mockL1ScrollMessenger
 	}
-	//TODO mainnet messenger address
 	throw new Error('Unsupported network')
 }
 
 export const getL2MessengerAddress = async () => {
 	if (network.name === 'scrollSepolia') {
-		return '0xBa50f5340FB9F3Bd074bD638c9BE13eCB36E603d' // real address
+		return '0xBa50f5340FB9F3Bd074bD638c9BE13eCB36E603d'
 	}
+	if (network.name === 'scroll') {
+		return '0x781e90f1c8Fc4611c9b7497C3B47F99Ef6969CbC'
+	}
+
+	// Get mock l2 messenger address from deployed contracts
 	const deployedContracts = await readDeployedContracts()
 	if (deployedContracts.mockL2ScrollMessenger) {
 		return deployedContracts.mockL2ScrollMessenger
